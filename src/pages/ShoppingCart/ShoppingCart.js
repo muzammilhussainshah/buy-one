3834aa841fd13d21a7e54b891de5db8b780780d7
Muzammil Hussain Shah <mynameismuzammilhussainshah@gmail.com>
2023-04-09 05:38:06 +0500

    'shoppingCardAdd'

--- src/pages/ShoppingCart/ShoppingCart.js
+++ src/pages/ShoppingCart/ShoppingCart.js
@@ -1,13 +1,15 @@
-import React, {
-  useState
-} from 'react';
-import MyButton from '../../components/MyButton';
-import { Cart, CheckBox, OrderHeader } from '../ConfirmAddress/Components/Components';
+import React from 'react';
+import {
+  Cart,
+  CheckBox,
+  OrderHeader
+} from '../ConfirmAddress/Components/Components';
 
-import '../ShoppingCart/style.css'
 import { CostContainer } from './Components/Components';
-import './style.css'
 import { SliderComp } from '../Home/Components/Components';
+import './style.css'
+import '../ShoppingCart/style.css'
+
 function ShoppingCart() {
 
   return (
@@ -28,32 +30,21 @@ function ShoppingCart() {
         <Cart checkBox label={`店鋪: 蘇寧易購官方旗艦店`} />
         <Cart checkBox label={`店鋪: 蘇寧易購官方旗艦店`} />
         <Cart checkBox label={`店鋪: 蘇寧易購官方旗艦店`} />
-
       </div >
-      {/* <div className='confirmAddressContainer' style={{ display: 'flex' }}> */}
       <div className='confirmAddressContainer createAddressSection ' >
-
-        {/* <div className='createAddressSection makeColumn' > */}
         <div className='cartHeading' style={{}} >
           <CheckBox
-            // state={checkState}
             label={`全選`}
             className="marginAdd1 orderHeaderBtn"
           />
           <p className='allProduct fontWeightNormal makeAnchr blue'>刪除</p>
           <p className='allProduct fontWeightNormal '>移入收藏夾</p>
-          {/* 購物車
-        <p className='allProduct fontWeightNormal'>（全部41）</p> */}
         </div>
         <div className='cartHeading '>
           <CostContainer />
         </div >
       </div>
-      {/* <div className='confirmAddressContainer' style={{ display: 'flex' }}> */}
       <SliderComp />
-      {/* </div> */}
-
-
     </>
   );
 }
